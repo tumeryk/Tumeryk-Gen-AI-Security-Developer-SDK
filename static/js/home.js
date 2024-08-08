973929c1e5c38022e3c81f5bc65300234a5250ee
Nirmit Ashar <13338417+nirmitashar@users.noreply.github.com>
2024-08-08 12:38:49 -0700

    made changes to fomratting of front-end

---
+++ static/js/home.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems);
+    
+    // Add event listener to the select element
+    document.getElementById('configSelect').addEventListener('change', function() {
+      var selectedConfig = this.value;
+
+      // Fetch the cookie value
+      var proxyCookie = document.cookie.split('; ').find(row => row.startsWith('proxy=')).split('=')[1];
+
+      fetch('/config_id?config_id=' + selectedConfig, {
+        method: 'GET',
+        credentials: 'same-origin',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${proxyCookie}`  // Include the proxy cookie in the headers
+        }
+      }).then(response => response.json())
+        .then(data => {
+          console.log('Config changed:', data);
+        }).catch(error => {
+          console.error('Error changing config:', error);
+        });
+    });
+  });
