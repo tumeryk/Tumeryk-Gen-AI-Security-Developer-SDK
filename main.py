608c3c4048c56e81991da1f22d71d1a8e443da42
Nirmit Ashar <13338417+nirmitashar@users.noreply.github.com>
2024-08-20 16:27:14 -0700

    Add main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from routers import auth, chat, config
+
+
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+# Include routers
+app.include_router(auth.router)
+app.include_router(chat.router)
+app.include_router(config.router)
+
+if __name__ == "__main__":
+    uvicorn.run(app="main:app", host="0.0.0.0", port=8000, reload=True)
