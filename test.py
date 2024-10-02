9c7af908eda6fc3ad7f4d7357b43474d8066dfcf
Nirmit Ashar <13338417+nirmitashar@users.noreply.github.com>
2024-10-02 11:45:58 -0700

    update requirements.txt

--- test.py
+++ test.py
@@ -1,92 +1,30 @@
-import time
 from tumeryk_proxy.api_client import ApiClient
-import os
 
-def test_api_client():
-    # Initialize the API client
-    url = os.getenv("BASE_URL")
-    username = os.getenv("USERNAME")
-    password = os.getenv("PASSWORD")
-    print(url,username,password)
-    api_client = ApiClient(url)
+client = ApiClient("https://chat-stage.tmryk.com")
+client.login("demoTumeryk", "abc123")
+client.set_config("guardrails_only")
 
-    # Step 1: Login with your username and password
-    login_response = api_client.login(username=username, password= password)
-    print(api_client.base_url)
-    print(
-        "Login Response:", login_response
-    )  # Check if login was successful# Step 2: Set the config ID to "hr_policy"
-    set_config_response = api_client.set_config(config_id="hr_policy")
-    print(
-        "Set Config Response:", set_config_response
-    )  # Check if config ID was set successfully# Step 3: Test the chat method to send a direct input to the LLM
-    chat_message = "How do you implement a binary tree in Python?"
-    start_time = time.time()
-    chat_response = api_client.chat(chat_message)
-    chat_response_time = time.time() - start_time
-    print(
-        "Chat Response:", chat_response
-    )  # Print the response from the LLM# Step 4: Test the chat_guard method to send input to the GuardAI service
-    guard_message = chat_message
-    start_time = time.time()
-    guard_response = api_client.chat_guard(guard_message)
-    guard_response_time = time.time() - start_time
-    print(
-        "Guard Response:", guard_response
-    )  # Print the response from the GuardAI service# Determine if there's a violation
+response = client.chat("I want to use a creditcard to make a payment.")
+# print(response)
+# print(response)
+# #It is impossible to determine your compensation without further information. Your compensation will depend. 16
+
+response2 = client.chat_guard("I want to use a creditcard to make a payment.")
+print(response2)
+# stats2 = response2
+# print(stats2)
+# #I'm sorry, I can't respond to that. 38
 
 
-def test_invalid_login(api_client):
-    try:
-        print("\nTesting invalid login credentials...")
-        login_response = api_client.login(username="wrongUser", password="wrongPass")
-        print("Invalid Login Response:", login_response)
-    except Exception as e:
-        print("Invalid login failed as expected:", e)
+# # completion_tokens_2 = int(re.search(r'(\d+) total tokens', stats2).group(1))
+# # print(f"Second response completion tokens: {completion_tokens_2}")
+
+# response3 = client.chat_guard("Generate a calculator program in Python")
+# print(response3)
+
+# #responses, time the number tokens for each
 
 
-def test_invalid_config_id(api_client):
-    try:
-        print("\nTesting invalid config ID...")
-        set_config_response = api_client.set_config(config_id="non_existent_config")
-        print("Invalid Set Config Response:", set_config_response)
-    except Exception as e:
-        print("Invalid config ID set failed as expected:", e)
 
 
-def test_invalid_chat_input():
-    try:
-        api_client = ApiClient()
-        print("\nTesting chat without logging in")
-        chat_message = ""
-        start_time = time.time()
-        chat_response = api_client.chat(chat_message)
-        chat_response_time = time.time() - start_time
-        print("Invalid Chat Response:", chat_response)
-    except Exception as e:
-        print("Invalid chat failed as expected:", e)
 
-
-def test_invalid_chat_guard_input():
-    try:
-        api_client = ApiClient()
-        print("\nTesting chat_guard")
-        guard_message = None
-        start_time = time.time()
-        guard_response = api_client.chat_guard(guard_message)
-        guard_response_time = time.time() - start_time
-        print("Invalid Guard Response:", guard_response)
-    except Exception as e:
-        print("Invalid guard chat failed as expected:", e)
-
-
-if __name__ == "__main__":
-    # Create a single instance of ApiClient
-    # api_client = ApiClient()
-
-    # Run tests using the shared instance
-    # test_invalid_login(api_client)
-    # test_invalid_config_id(api_client)
-    # test_invalid_chat_input()
-    # test_invalid_chat_guard_input()
-    test_api_client()
